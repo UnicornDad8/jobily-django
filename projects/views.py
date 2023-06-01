b9760bda902368753d8b190086db650cb7348fb1
UnicornDad8 <unicorndad8@gmail.com>
2023-06-01 14:24:46 -0400

    Started using model forms and created a form template for the projects

--- projects/views.py
+++ projects/views.py
@@ -1,23 +1,6 @@
 from django.shortcuts import render
 from .models import Project
-
-projectsList = [
-    {
-        'id': '1',
-        'title': "Ecommerce Website",
-        'description': 'Fully functional ecommerce America website'
-    },
-    {
-        'id': '2',
-        'title': "Portfolio Website",
-        'description': 'This was a project where I built out my portfolio'
-    },
-    {
-        'id': '3',
-        'title': "Social Network",
-        'description': 'Awesome open source project I am still working on it'
-    }
-]
+from .forms import ProjectForm
 
 
 def projects(request):
@@ -31,3 +14,9 @@ def projects(request):
 def project(request, pk):
     projectObj = Project.objects.get(id=pk)
     return render(request, 'projects/single-project.html', {"project": projectObj})
+
+
+def createProject(request):
+    form = ProjectForm()
+    context = {"form": form}
+    return render(request, "projects/project_form.html", context)
