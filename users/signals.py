38be0477876e11988a44750a850be11ecbdb6f92
UnicornDad8 <unicorndad8@gmail.com>
2023-06-19 15:36:12 -0400

    Configure emails in settings, fixed SSL verification error, generated EMAIL_HOST_PASSWORD from google account, and finally added welcome email functionality when register a user

--- users/signals.py
+++ users/signals.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from .models import Profile
+from django.core.mail import send_mail
+from django.conf import settings
 
 # @receiver(post_save, sender=Profile)
 
@@ -14,6 +16,16 @@ def create_profile(sender, instance, created, **kwargs):
             username=user.username,
             email=user.email,
             name=user.first_name,
+        )
+        subject = 'Welcome to Jobily'
+        message = 'We are glad you chose us!'
+
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False,
         )
 
 
